664bdea1d1e7e39c195fb592d07bc495cb544b48
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-09-02 23:01:36 +0900

    improvement: select all levels as default

--- lib/local-storage/get-selected-levels.ts
+++ lib/local-storage/get-selected-levels.ts
@@ -1,4 +1,11 @@
+const DEFAULT_SELECTED_LEVELS = ["1", "2", "3", "4", "5"];
+
 export function getSelectedLevels() {
   const selectedLevelsStr = localStorage.getItem("selectedLevels");
+
+  if (selectedLevelsStr === null) {
+    return DEFAULT_SELECTED_LEVELS;
+  }
+
   return selectedLevelsStr ? selectedLevelsStr.split(",") : [];
 }
