819484f6e1fd0d15a94893e0b0c9fc3d4df52f10
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-09-02 22:03:10 +0900

    improvement: select eiken tag as default temporarily

--- lib/local-storage/get-selected-tags.ts
+++ lib/local-storage/get-selected-tags.ts
@@ -1,4 +1,11 @@
+const DEFAULT_SELECTED_TAGS = ["英検"];
+
 export function getSelectedTags() {
   const selectedTagsStr = localStorage.getItem("selectedTags");
+
+  if (selectedTagsStr === null) {
+    return DEFAULT_SELECTED_TAGS;
+  }
+
   return selectedTagsStr ? selectedTagsStr.split(",") : [];
 }
